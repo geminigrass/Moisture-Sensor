697f61b1fa2309117a5558bbeafc64f23e3eaf84
canl2 <canl2@andrew.cmu.edu>
2018-04-28 03:09:05 -0400

    try gpio

--- hello-flask.py
+++ hello-flask.py
@@ -1,29 +1,40 @@
-
-
-'''
 from flask import Flask
+import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
+import smtplib # This is the SMTP library we need to send the email notification
+import time # This is the time library, we need this so we can use the sleep function
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
-'''
+def callback(channel):
+	if GPIO.input(channel):
+		print ("LED off")
+
+	else:
+		print ("LED on")
 
 
-from flask import Flask
-from flask_restful import Resource, Api
+@app.route('/')
+def index():
+    while True:
+        # This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
+        time.sleep(0.1)
+        print("hello")
+    return 'Hello world'
 
-app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+GPIO.setmode(GPIO.BCM)
 
-api.add_resource(HelloWorld, '/')
+# Define the GPIO pin that we have our digital output from our sensor connected to
+channel = 17
+# Set the GPIO pin to an input
+GPIO.setup(channel, GPIO.IN)
+
+# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
+GPIO.add_event_callback(channel, callback)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
